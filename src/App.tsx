04695748105cf7cfb31495f582a71f31e78167cf
pedro <pedrorf.dev@gmail.com>
2025-02-24 11:45:29 -0300

    Feat: Adding Styled Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { Home, Leads, Login, Profile, Registration } from './pages'
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<>Login</>} />
-        <Route path="/cadastro" element={<>CADASTRO</>} />
-        <Route path="/home" element={<>Home</>} />
-        <Route path="/leads" element={<>Leads</>} />
-        <Route path="/perfil" element={<>Perfil</>} />
+        <Route path="/" element={<Login />} />
+        <Route path="/cadastro" element={<Registration />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/leads" element={<Leads />} />
+        <Route path="/perfil" element={<Profile />} />
       </Routes>
     </Router>
   )
