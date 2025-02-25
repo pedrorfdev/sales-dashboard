eff6aa2194b836550732073876051bd4946db165
pedro <pedrorf.dev@gmail.com>
2025-02-25 15:49:51 -0300

    Feat: Chart Component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
-export { default as CustomTable } from './CustomTable'
 export { default as AvatarsList } from './AvatarList'
+export { default as CustomChart } from './CustomChart'
+export { default as CustomTable } from './CustomTable'
 export { default as FormComponent } from './FormComponent'
 export * from './BannerImage'
 export * from './StyledButton'
