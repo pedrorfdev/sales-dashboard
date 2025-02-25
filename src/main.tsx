af97b0ec243b654c4d5eac7fca14a162533d1593
pedro <pedrorf.dev@gmail.com>
2025-02-25 15:51:41 -0300

    Fix: Fixing unused imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { darkTheme, GlobalStyle, lightTheme } from './styles'
+import { GlobalStyle, lightTheme } from './styles'
 import App from './App.tsx'
 import { ThemeProvider } from 'styled-components'
 
